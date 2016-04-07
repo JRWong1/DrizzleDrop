50d5911483966f8d17efc6d8886c3e19cad4019b
David Kluszczynski <dave07747@gmail.com>
2016-04-07 19:52:49 -0400

    Calendar date picker

--- DrizzleDrop/app/src/main/java/com/drizzledrop/drizzledrop/MainActivity.java
+++ DrizzleDrop/app/src/main/java/com/drizzledrop/drizzledrop/MainActivity.java
@@ -77,18 +77,16 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 2;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Train";
                 case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
+                    return "Bus";
             }
             return null;
         }
@@ -124,7 +122,6 @@ public class MainActivity extends AppCompatActivity {
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
     }
